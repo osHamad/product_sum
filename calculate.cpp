96afc2d3a284cf25e9de2e4120fbed7c4abce4b8
Osama Hamad <osamahama2005@gmail.com>
2021-10-14 13:40:27 -0400

    add main function

---
+++ calculate.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Product Sum Calculator\n";
+	std::cout << "Insert Product: ";
+	std::cin >> product >> "\n";
+	std::cout << "Insert Sum: ";
+	std::cin >> sum >> "\n";
+	return 0;
+}
