10daedfb6b7cea1e398f13436aad171a12ffb2e1
Osama Hamad <osamahama2005@gmail.com>
2021-10-14 15:04:24 -0400

    add product sum function

--- calculate.cpp
+++ calculate.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
+void product_sum(int product, int sum){
+	for (int i = 0; i <= product; i++){
+		std::cout << i;
+	}
+	std::cout << product << ", " << sum;
+}
+
 int main(){
+	// this is where aour inputs will be stored
+	int product;
+	int sum;
+
+	// recording inputs for sum and product
 	std::cout << "Product Sum Calculator\n";
 	std::cout << "Insert Product: ";
-	std::cin >> product >> "\n";
+	std::cin >> product;
 	std::cout << "Insert Sum: ";
-	std::cin >> sum >> "\n";
+	std::cin >> sum;
+	product_sum(product, sum);
+
 	return 0;
 }
